a6c4690fe3e33248569e42f6f5fb407c69b91cc5
Mark C Allen <mark@markcallen.com>
2023-04-06 12:40:50 -0700

    Added dvc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+//import type { AppProps } from 'next/app';
+import { withDVCProvider } from '@devcycle/devcycle-react-sdk';
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: any) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default withDVCProvider({
+  sdkKey: 'client-8e6ab66d-a40b-438e-bb17-d7fd5d7eb391'
+})(App);
