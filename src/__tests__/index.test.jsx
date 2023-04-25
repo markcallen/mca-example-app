fba67d7584384afa775b1a4095a3fa2e976046d0
Mark C Allen <mark@markcallen.com>
2023-04-25 11:27:33 -0700

    Added jest and sample test

---
+++ src/__tests__/index.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    expect(document.title).toEqual('');
+  });
+});
