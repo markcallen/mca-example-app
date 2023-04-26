d2d5b7c46e600da1172b7fe0a64137a82f9498e1
Mark C Allen <mark@markcallen.com>
2023-04-26 16:06:03 -0700

    Added dvcVariable to change the hero logo when using new-user-id.  Added messaging to receive a request for variables and send them back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,45 @@
+import React, { useEffect } from 'react';
+
 import { ChakraProvider } from '@chakra-ui/react';
-import { withDVCProvider } from '@devcycle/devcycle-react-sdk';
+import { useDVCClient, withDVCProvider } from '@devcycle/devcycle-react-sdk';
 
 const App = ({ Component, pageProps }: any) => {
+  const dvcClient = useDVCClient();
+
+  useEffect(() => {
+    window.addEventListener(
+      'message',
+      function (e) {
+        // if (e.origin !== 'http://localhost:3001') {
+        //  // console.log("wrong");
+        //  return;
+        //}
+        try {
+          if (
+            e.data &&
+            e.data.type === '__DEVCYCLE_DEVTOOL_GLOBAL_HOOK_EVENT'
+          ) {
+            if (e.data.subType === 'GET_VARIABLES') {
+              console.log('react-app: GET_VARIABLES');
+              window.postMessage(
+                {
+                  type: '__DEVCYCLE_DEVTOOL_GLOBAL_HOOK_EVENT',
+                  subType: 'VARIABLES',
+                  data: JSON.stringify(dvcClient.allVariables())
+                },
+                '*'
+              );
+            }
+          }
+        } catch (err) {
+          console.log(e);
+          console.error(err);
+        }
+      },
+      false
+    );
+  }, [dvcClient]);
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
